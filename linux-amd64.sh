60495e8a1ea338e300f372268fcabf0a0659f2eb
Rimolde Technology Services <130383413+rimoldetech@users.noreply.github.com>
2023-04-17 16:55:31 -0500

    Create linux-amd64.sh

---
+++ linux-amd64.sh
@@ -0,0 +1,192 @@
+#!/usr/bin/env bash
+
+if [ $EUID -ne 0 ]; then
+  echo "ERROR: Must be run as root"
+  exit 1
+fi
+
+HAS_SYSTEMD=$(ps --no-headers -o comm 1)
+if [ "${HAS_SYSTEMD}" != 'systemd' ]; then
+    echo "This install script only supports systemd"
+    echo "Please install systemd or manually create the service using your systems's service manager"
+    exit 1
+fi
+
+agentDL='https://agents.tacticalrmm.com/api/v2/agents/?version=2.4.6&arch=amd64&token=aa028d46-6b91-4a1c-8686-35e947528067&plat=linux&api=api.tactical-rts.com'
+meshDL='https://mesh.tactical-rts.com/meshagents?id=bq3U5aIE4ZEMXuv@ybXgQrOtV9CUOI6ilbNpV8IQUNvVIfnYTk6FKlppYB7Q7@xt&installflags=2&meshinstall=6'
+
+apiURL='https://api.tactical-rts.com'
+token='509ca4990f42d738fd4fdf3900e104c5099174def535642d7c311e9f7a6d9cea'
+clientID='6'
+siteID='7'
+agentType='workstation'
+proxy=''
+
+agentBinPath='/usr/local/bin'
+binName='tacticalagent'
+agentBin="${agentBinPath}/${binName}"
+agentConf='/etc/tacticalagent'
+agentSvcName='tacticalagent.service'
+agentSysD="/etc/systemd/system/${agentSvcName}"
+meshDir='/opt/tacticalmesh'
+meshSystemBin="${meshDir}/meshagent"
+meshSvcName='meshagent.service'
+meshSysD="/lib/systemd/system/${meshSvcName}"
+
+deb=(ubuntu debian raspbian kali linuxmint)
+rhe=(fedora rocky centos rhel amzn arch opensuse)
+
+set_locale_deb() {
+locale-gen "en_US.UTF-8"
+localectl set-locale LANG=en_US.UTF-8
+. /etc/default/locale
+}
+
+set_locale_rhel() {
+localedef -c -i en_US -f UTF-8 en_US.UTF-8 > /dev/null 2>&1
+localectl set-locale LANG=en_US.UTF-8
+. /etc/locale.conf
+}
+
+RemoveOldAgent() {
+    if [ -f "${agentSysD}" ]; then
+        systemctl disable ${agentSvcName}
+        systemctl stop ${agentSvcName}
+        rm -f ${agentSysD}
+        systemctl daemon-reload
+    fi
+
+    if [ -f "${agentConf}" ]; then
+        rm -f ${agentConf}
+    fi
+
+    if [ -f "${agentBin}" ]; then
+        rm -f ${agentBin}
+    fi
+}
+
+InstallMesh() {
+    if [ -f /etc/os-release ]; then
+        distroID=$(. /etc/os-release; echo $ID)
+        distroIDLIKE=$(. /etc/os-release; echo $ID_LIKE)
+        if [[ " ${deb[*]} " =~ " ${distroID} " ]]; then
+            set_locale_deb
+        elif [[ " ${deb[*]} " =~ " ${distroIDLIKE} " ]]; then
+            set_locale_deb
+        elif [[ " ${rhe[*]} " =~ " ${distroID} " ]]; then
+            set_locale_rhel
+        else
+            set_locale_rhel
+        fi
+    fi
+
+    meshTmpDir=$(mktemp -d -t "mesh-XXXXXXXXX")
+    if [ $? -ne 0 ]; then
+        meshTmpDir='/root/meshtemp'
+        mkdir -p ${meshTmpDir}
+    fi
+    meshTmpBin="${meshTmpDir}/meshagent"
+    wget --no-check-certificate -q -O ${meshTmpBin} ${meshDL}
+    chmod +x ${meshTmpBin}
+    mkdir -p ${meshDir}
+    env LC_ALL=en_US.UTF-8 LANGUAGE=en_US XAUTHORITY=foo DISPLAY=bar ${meshTmpBin} -install --installPath=${meshDir}
+    sleep 1
+    rm -rf ${meshTmpDir}
+}
+
+RemoveMesh() {
+    if [ -f "${meshSystemBin}" ]; then
+        env XAUTHORITY=foo DISPLAY=bar ${meshSystemBin} -uninstall
+        sleep 1
+    fi
+
+    if [ -f "${meshSysD}" ]; then
+        systemctl stop ${meshSvcName} > /dev/null 2>&1
+        systemctl disable ${meshSvcName} > /dev/null 2>&1
+        rm -f ${meshSysD}
+    fi
+
+    rm -rf ${meshDir}
+    systemctl daemon-reload
+}
+
+Uninstall() {
+    RemoveMesh
+    RemoveOldAgent
+}
+
+if [ $# -ne 0 ] && [ $1 == 'uninstall' ]; then
+    Uninstall
+    exit 0
+fi
+
+RemoveOldAgent
+
+echo "Downloading tactical agent..."
+wget -q -O ${agentBin} "${agentDL}"
+if [ $? -ne 0 ]; then
+    echo "ERROR: Unable to download tactical agent"
+    exit 1
+fi
+chmod +x ${agentBin}
+
+MESH_NODE_ID=""
+
+if [ $# -ne 0 ] && [ $1 == '--nomesh' ]; then
+    echo "Skipping mesh install"
+else
+    if [ -f "${meshSystemBin}" ]; then
+        RemoveMesh
+    fi
+    echo "Downloading and installing mesh agent..."
+    InstallMesh
+    sleep 2
+    echo "Getting mesh node id..."
+    MESH_NODE_ID=$(env XAUTHORITY=foo DISPLAY=bar ${agentBin} -m nixmeshnodeid)
+fi
+
+if [ ! -d "${agentBinPath}" ]; then
+    echo "Creating ${agentBinPath}"
+    mkdir -p ${agentBinPath}
+fi
+
+if [ $# -ne 0 ] && [ $1 == '--debug' ]; then
+    INSTALL_CMD="${agentBin} -m install -api ${apiURL} -client-id ${clientID} -site-id ${siteID} -agent-type ${agentType} -auth ${token} -log debug"
+else
+    INSTALL_CMD="${agentBin} -m install -api ${apiURL} -client-id ${clientID} -site-id ${siteID} -agent-type ${agentType} -auth ${token}"
+fi
+
+if [ "${MESH_NODE_ID}" != '' ]; then
+    INSTALL_CMD+=" -meshnodeid ${MESH_NODE_ID}"
+fi
+
+if [ "${proxy}" != '' ]; then
+    INSTALL_CMD+=" -proxy ${proxy}"
+fi
+
+eval ${INSTALL_CMD}
+
+tacticalsvc="$(cat << EOF
+[Unit]
+Description=Tactical RMM Linux Agent
+
+[Service]
+Type=simple
+ExecStart=${agentBin} -m svc
+User=root
+Group=root
+Restart=always
+RestartSec=5s
+LimitNOFILE=1000000
+KillMode=process
+
+[Install]
+WantedBy=multi-user.target
+EOF
+)"
+echo "${tacticalsvc}" | tee ${agentSysD} > /dev/null
+
+systemctl daemon-reload
+systemctl enable ${agentSvcName}
+systemctl start ${agentSvcName}
+
